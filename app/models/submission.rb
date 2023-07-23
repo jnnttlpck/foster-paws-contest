cdcd8f530b8dbe4973297166be6907a2314a2585
jnnttlpck <jeannettepeck@gmail.com>
2023-07-23 14:13:37 -0400

    Add validations to submission incl file image format

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,12 +1,17 @@
 class Submission < ApplicationRecord
-    has_one :order
+    has_one :order, dependent: :destroy
     has_one_attached :file do |attachable|
         attachable.variant :medium, resize_to_limit: [800, 800]
         attachable.variant :small, resize_to_limit: [300,300]
     end
 
-    validates :email, presence: true
-
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :location, presence: true
+    validates :pet_name, presence: true
+    validates :file, presence: true
+    validates :email, presence: true, format: URI::MailTo::EMAIL_REGEXP
+    validate :file_image_format
     enum status: {
         open: 0,
         complete: 1,
@@ -14,4 +19,14 @@ class Submission < ApplicationRecord
     }
 
     accepts_nested_attributes_for :order
+
+    private
+
+    def file_image_format
+        return unless file.attached?
+        return if file.blob.content_type.starts_with?('image/')
+
+        file.purge
+        errors.add(:base, 'File must be an image.') and return false
+    end
 end
