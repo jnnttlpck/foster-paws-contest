5d08c095b0c418c83a07ce29f5eec0dee5dcf781
jnnttlpck <jeannettepeck@gmail.com>
2023-07-25 23:18:47 -0400

    Fix stripe attribute name

--- app/helpers/submission_helper.rb
+++ app/helpers/submission_helper.rb
@@ -1,6 +1,6 @@
 module SubmissionHelper
     def formatted_transaction_fee(base_price, transaction_price)
-        stripe_base_price = Stripe::Price.retrieve(base_price.stripe_key).unit_price
+        stripe_base_price = Stripe::Price.retrieve(base_price.stripe_key).unit_amount
         stripe_transaction_price = Stripe::Price.retrieve(transaction_price.stripe_key).unit_amount
         
         diff = stripe_transaction_price - stripe_base_price
