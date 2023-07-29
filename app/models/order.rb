d3fa1676710ca6132e20b76462ca2329dd2642be
jnnttlpck <jeannettepeck@gmail.com>
2023-07-29 12:34:55 -0400

    Rmv email validation from order (on user now)

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,7 +3,6 @@ class Order < ApplicationRecord
   belongs_to :submission, optional: true
   has_many :line_items, dependent: :destroy
 
-  validates :email, presence: true, format: URI::MailTo::EMAIL_REGEXP
   validates_presence_of :name, :line_1, :city, :state, :zip
 
   enum status: {
