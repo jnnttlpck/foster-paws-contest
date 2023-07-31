cd14a233f816b8196eaf190e32a09834beb0043a
jnnttlpck <jeannettepeck@gmail.com>
2023-07-30 20:56:42 -0400

    comment adding stripe env

--- config/environment.rb
+++ config/environment.rb
@@ -4,5 +4,5 @@ require_relative "application"
 # Initialize the Rails application.
 Rails.application.initialize!
 
-stripe_env_vars = File.join(Rails.root, 'config', 'stripe_keys.yml')
-YAML.load(stripe_env_vars) if File.exists?(stripe_env_vars)
+# stripe_env_vars = File.join(Rails.root, 'config', 'stripe_keys.yml')
+# YAML.load(stripe_env_vars) if File.exists?(stripe_env_vars)
