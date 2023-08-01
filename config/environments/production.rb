2375559570ec13d1a360d73ec024297d80b83db9
jnnttlpck <jeannettepeck@gmail.com>
2023-08-01 13:00:48 -0400

    temp: test with tmp host name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_controller.default_url_options = { host: 'cutesccats.com' }
+  config.action_controller.default_url_options = { host: 'rails-zsel.onrender.com' }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
